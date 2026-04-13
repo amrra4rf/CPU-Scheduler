479bf4b59193426bb56cfaba98afdb1d102c7ca4
Joex-stack <yousssefa7med@gmail.com>
2026-04-13 17:44:44 +0200

    Modify models

--- Models/Scheduler.hpp
+++ Models/Scheduler.hpp
@@ -7,14 +7,16 @@
 class Scheduler
 {
 protected:
-    int time_quantum = -1;
+    //int time_quantum = -1;
 
 public:
 
     virtual void addProcess(const Process& p) = 0;
-    virtual int step() = 0;  
+    virtual int Schdule() = 0;  
     virtual bool isFinished() const = 0;
-    virtual void set_qt(int q)=0;
+    virtual double Average_wait_time()const =0;
+    virtual double Average_turnaround_time()const =0; 
+    //virtual void set_qt(int q)=0;
 
     
 };
