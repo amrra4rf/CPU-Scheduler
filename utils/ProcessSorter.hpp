c1a1a902b2c2144ff9e3bce67c88c705716b606c
Joex-stack <yousssefa7med@gmail.com>
2026-04-13 18:22:42 +0200

    add utils

---
+++ utils/ProcessSorter.hpp
@@ -0,0 +1,66 @@
+#ifndef PROCESS_SORTER_HPP
+#define PROCESS_SORTER_HPP
+
+#include <queue>
+#include <vector>
+#include <stdexcept>
+#include "../Models/Process.hpp"
+#include "../Models/Scheduler.hpp"
+
+struct pcomparator
+{
+    bool operator()(const Process& p1, const Process& p2) const
+    {
+        // min-heap by arrival time
+        return p1.getArrival() > p2.getArrival();
+    }
+};
+
+class ProcessSorter
+{
+private:
+    std::priority_queue<
+        Process,
+        std::vector<Process>,
+        pcomparator
+    > pq;
+
+public:
+    void addProcess_sorted(const Process& p)
+    {
+        pq.push(p);
+    }
+
+    bool empty() const
+    {
+        return pq.empty();
+    }
+
+    Process getNext()
+    {
+        if (pq.empty())
+            throw std::runtime_error("ProcessSorter is empty");
+
+        Process top = pq.top();
+        pq.pop();
+        return top;
+    }
+
+    Process peek() const
+    {
+        if (pq.empty())
+            throw std::runtime_error("ProcessSorter is empty");
+
+        return pq.top();
+    }
+
+    void transferToScheduler(Scheduler& s)
+    {
+        while (!empty())
+        {
+            s.addProcess(getNext());
+        }
+    }
+};
+
+#endif 
