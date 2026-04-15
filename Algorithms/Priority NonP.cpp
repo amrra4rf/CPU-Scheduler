f5fd3d30c0e7d9226ca5f1d09635019e09acc40e
Joex-stack <yousssefa7med@gmail.com>
2026-04-15 16:12:47 +0200

    Add pri non

--- Algorithms/Priority NonP.cpp
+++ Algorithms/Priority NonP.cpp
@@ -12,6 +12,8 @@ private:
     PrioritySorter readyP;
     std::vector<Process> finished;
     int current_time = 0;
+    Process current_process;
+    bool hasCurrent = false;
 
 public:
     void addProcess(const Process& p) override
@@ -27,25 +29,35 @@ public:
     }
 
     int schedule() override
+{
+
+    if (!hasCurrent)
     {
         if (readyP.empty())
             return -1;
 
-        Process p = readyP.getNextProcess();
+        current_process = readyP.getNextProcess();
 
+        if (current_time < current_process.getArrival())
+            current_time = current_process.getArrival();
 
-        if (current_time < p.getArrival())
-            current_time = p.getArrival();
-
-        p.start_p(current_time);
-
-        current_time += p.getBurst();
-        p.finish(current_time);
-
-        finished.push_back(p);
-
-        return p.getPID();
+        current_process.start_p(current_time);
+        hasCurrent = true;
     }
+
+    current_process.execute(1);
+    current_time++;
+
+
+    if (current_process.getRemaining() == 0)
+    {
+        current_process.finish(current_time);
+        finished.push_back(current_process);
+        hasCurrent = false;
+    }
+
+    return current_process.getPID();
+}
 
     bool isFinished() const override
     {
